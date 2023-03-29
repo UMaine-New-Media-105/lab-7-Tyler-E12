4346ee89e1cb446c57dc0c528fee366c20fba7b7
Tyler-E12 <124187250+Tyler-E12@users.noreply.github.com>
2023-03-29 12:38:57 -0400

    Create exerciseThree.js
    Creates a 400x400 canvas with a grid of blue circles filling it, with a blue gradient. If the mouse is pressed and held, the gradient's value inverts, changing the colors.

---
+++ exerciseThree.js
@@ -0,0 +1,37 @@
+let xCoord = 25;
+let yCoord = 27;
+let offset = 50;
+let colorChange = 1;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  let R = 100;
+  let G = 150;
+  let B = 230;
+
+  for (let numRows = 0; numRows < 10; numRows++) {
+    for (let numCols = 0; numCols < 10; numCols++) {
+      drawSprite(numRows * offset + 25, numCols * offset + 27, R, G, B);
+      if (mouseIsPressed) {
+        R = R + colorChange;
+        G = G + colorChange;
+        B = B + colorChange;
+      } else {
+        R = R - colorChange;
+        G = G - colorChange;
+        B = B - colorChange;
+      }
+    }
+  }
+}
+
+function drawSprite(x, y, a, b, c) {
+  push();
+  fill(a, b, c);
+  ellipse(x, y, 50);
+  pop();
+}
